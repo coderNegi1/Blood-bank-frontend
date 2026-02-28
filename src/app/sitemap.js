8101c35bde03948fa526b5be37b58347ebbc57be
amannegidev <amannegi2314@gmail.com>
2026-02-28 09:58:03 +0530

    sitemap

---
+++ src/app/sitemap.js
@@ -0,0 +1,12 @@
+export default function sitemap() {
+  return [
+    { url: "https://www.ukbloodcentre.com", priority: 1.0 },
+
+    { url: "https://www.ukbloodcentre.com/about-us", priority: 0.8 },
+
+    { url: "https://www.ukbloodcentre.com/donation-process", priority: 0.9 },
+
+    { url: "https://www.ukbloodcentre.com/contact", priority: 0.9 },
+
+  ];
+}
